08e0eb93ec67ca189b3eb7d1c7c4f47363ebc32b
ld_xu <ld_xu@trip.com>
2023-10-03 17:35:49 +0800

    feat: 实现redux源码

--- 09_ React框架原理与实战/04_myRedux/index.js
+++ 09_ React框架原理与实战/04_myRedux/index.js
@@ -1,4 +1,15 @@
-function createStore(reudcer, initilaState) {
+function createStore(reducer, initilaState, enhancer) {
+  // 约束reducer参数类型
+  if (typeof reducer !== "function") {
+    throw new Error("reducer必须是函数");
+  }
+
+  if (typeof enhancer !== "undefined") {
+    if (typeof enhancer !== "function") {
+      throw new Error("enhancer必须是函数");
+    }
+    return enhancer(createStore)(reducer, initilaState);
+  }
   var currentState = initilaState;
 
   var currentListeners = [];
@@ -8,7 +19,13 @@ function createStore(reudcer, initilaState) {
   }
 
   function dispatch(action) {
-    currentState = reudcer(currentState, action);
+    if (!isPlainObject(action)) {
+      throw new Error("action必须是对象");
+    }
+    if (typeof action.type === "undefined") {
+      throw new Error("action必须要有type属性");
+    }
+    currentState = reducer(currentState, action);
 
     for (let i = 0; i < currentListeners.length; i++) {
       let listener = currentListeners[i];
@@ -26,4 +43,58 @@ function createStore(reudcer, initilaState) {
     dispatch,
     subscribe,
   };
+}
+
+// 判断是否是对象
+function isPlainObject(obj) {
+  // 排查基础数据类型和null
+  if (typeof obj !== "object" || obj === null) {
+    return false;
+  }
+  // 区分数组和对象 原型对象对比的方式
+  let proto = obj;
+  while (Object.getPrototypeOf(proto) != null) {
+    proto = Object.getPrototypeOf(proto);
+  }
+  return Object.getPrototypeOf(obj) === proto;
+}
+
+function applyMiddleware(...middlewares) {
+  return function (createStore) {
+    return function (reducer, initialState) {
+      const store = createStore(reducer, initialState);
+      const middlewareApi = {
+        getState: store.getState,
+        dispatch: store.dispatch,
+      };
+      const chain = middlewares.map((middleware) => middleware(middlewareApi));
+      let dispatch = compose(...chain)(store.dispatch);
+      return {
+        ...store,
+        dispatch,
+      };
+    };
+  };
+}
+
+function compose() {
+  let fns = [...arguments];
+  return function (dispatch) {
+    for (let i = fns.length - 1; i >= 0; i--) {
+      dispatch = fns[i](dispatch);
+    }
+    return dispatch;
+  };
+}
+
+function bindActionCreators(actionCreators, dispatch) {
+  let boundActionCreators = {};
+
+  for (let key in actionCreators) {
+    boundActionCreators[key] = function () {
+      dispatch(actionCreators[key]());
+    };
+  }
+
+  return boundActionCreators;
 }
