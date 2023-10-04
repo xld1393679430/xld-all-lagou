348fe9932ba252a7b1ad07ee6cb1b0f32cab80e1
ld_xu <ld_xu@trip.com>
2023-10-04 15:36:18 +0800

    feat: 异步action

--- 09_ React框架原理与实战/07_mobx-v5/src/components/Counter/index.js
+++ 09_ React框架原理与实战/07_mobx-v5/src/components/Counter/index.js
@@ -1,17 +1,29 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
+import { toJS } from "mobx";
 import { inject, observer } from "mobx-react";
 
 @inject("counter")
 @observer
 class Index extends Component {
+  componentDidMount() {
+    const { getData } = this.props.counter;
+    getData();
+  }
+
   render() {
-    console.log(222, this.props.counter);
-    const { count, increment, decrement } = this.props.counter;
+    const { count, users, increment, decrement } = this.props.counter;
+    const _users = toJS(users);
     return (
       <div>
         <button onClick={increment}>+1</button>
         <span>{count}</span>
         <button onClick={decrement}>-1</button>
+        <hr />
+        <ul>
+          {_users.map((item) => {
+            return <li key={item.id}>{item.login}</li>;
+          })}
+        </ul>
       </div>
     );
   }
--- 09_ React框架原理与实战/07_mobx-v5/src/store/counter.js
+++ 09_ React框架原理与实战/07_mobx-v5/src/store/counter.js
@@ -1,21 +1,33 @@
-import { action, observable } from "mobx"
-import { observer } from "mobx-react"
+import { action, configure, observable, runInAction } from "mobx";
+
+// 通过配置强制程序使用action函数更改应用程序中的状态
+configure({ enforceActions: "observed" });
 
 class Counter {
-	@observable 
-	count = 0
+  @observable count = 0;
 
-	@action
-	increment = () => {
-		this.count += 1
-	}
+  @observable users = [];
 
-	@action
-	decrement = () => {
-		this.count -= 1
-	}
+  @action
+  increment = () => {
+    this.count += 1;
+  };
+
+  // 可以通过bound 或是使用箭头函数绑定this
+  @action.bound
+  decrement() {
+    this.count -= 1;
+  }
+
+  @action.bound async getData() {
+    const users = await fetch("https://api.github.com/users").then((res) => res.json());
+    // 异步action改变observable需要在runInAction回调里操作
+	runInAction(() => {
+      this.users = users;
+    });
+  }
 }
 
-const counter = new Counter()
+const counter = new Counter();
 
 export default counter;
