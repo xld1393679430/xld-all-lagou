53623b394cbe615fddabedb43b4974dda196d642
ld_xu <ld_xu@trip.com>
2023-10-05 15:09:11 +0800

    feat: formik as 用法

--- 09_ React框架原理与实战/09_formik/src/components/FormikComponent/index.js
+++ 09_ React框架原理与实战/09_formik/src/components/FormikComponent/index.js
@@ -4,10 +4,12 @@ import * as Yup from "yup";
 const Index = () => {
   const initialValues = {
     username: "",
+    password: "",
+    content: "content",
   };
 
   const handleSubmit = (values) => {
-    console.log(values, "handleSubmit");
+    alert(JSON.stringify(values));
   };
 
   const schema = Yup.object({
@@ -18,9 +20,26 @@ const Index = () => {
     <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={schema}>
       <Form>
         <div>
-          <Field name="username" />
+          <label htmlFor="username">username: </label>
+          <Field name="username" id="username" />
           <p>
             <ErrorMessage name="username" />
+          </p>
+        </div>
+
+        <div>
+          <label htmlFor="password">password: </label>
+          <Field name="password" id="password" type="password" />
+          <p>
+            <ErrorMessage name="password" />
+          </p>
+        </div>
+
+        <div>
+          <label htmlFor="content">content: </label>
+          <Field name="content" id="content" as="textarea" />
+          <p>
+            <ErrorMessage name="content" />
           </p>
         </div>
 
